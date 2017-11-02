8167ed206c6841a948d9d6f4713c7bcfd37796a7
Cherita Ousley <cheritaousley@Cheritas-MacBook-Air.local>
2017-11-02 11:51:47 -0500

    updating with CRUD assignment

---
+++ Simple_CRUD/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using DbConnection;
+using System.Collections.Generic;
+
+namespace Simple_CRUD
+{
+    class Program
+    {
+        static public void Read()
+        {
+            List<Dictionary<string, object>> userInfo = DbConnector.Query("SELECT FirstName as 'First Name', LastName as 'Last Name', FavoriteNum as 'Favorite Number' from User");
+            Console.WriteLine("User Information as follows:");
+            foreach (var info in userInfo)
+            {
+                foreach (KeyValuePair<string, object> user in info)
+                    Console.Write("{user.Key}: {user.Value}" + "");
+            }
+            Console.WriteLine();
+        }
+        static public void Create()
+        {
+            // string first = Console.ReadLine();
+            // string last = Console.ReadLine();
+            // string number = Console.ReadLine();
+            
+            //  OR you could do this 
+            string first = "";
+            string last = "";
+            string number = "";
+            string Id ="";
+            //and use readLine below
+
+            // string first = "first";//Console.ReadLine();
+            // string last = "last"; //Console.ReadLine();
+            // string number = "10";//Console.ReadLine();
+
+        //the $ sign tells the program I want to insert some variables so the first name, last name and favnum become a different color indicating they're variables
+            //(Opitonal) update and delete
+            Console.Write("Please enter the ID of the record you wish to update");
+            Id = Console.ReadLine();
+            string selectquery = $"select * from users where id = {Id}";
+            List<Dictionary<string, object>> result =DbConnector.Query(selectquery);
+
+            if(result.Count > 0)
+            {
+                Console.Write("Great, I have found that id, what is the new first name? :");
+                Console.WriteLine("1: Update the user");
+                Console.WriteLine("2: Delete the user");
+                Console.Write("Choose :");
+                string answer = Console.ReadLine();
+                if(answer.Trim()== "1")
+                {
+                    first = Console.ReadLine();
+                    Console.Write("Got it. Now tell me the new last name:");
+                    last = Console.ReadLine();
+                    Console.Write("Very good. What is this person's new fav number?");
+                    number = Console.ReadLine();
+
+                    string updateQuery = $"update users set FirstName = '{first}', LastName = '{last}', FavNum ='{number}'";
+                    DbConnector.Query(updateQuery);
+
+                    Console.Write("Success!");
+                }
+                else if(answer.Trim()== "2")
+                {
+                    string deleteQuery =$"delete from User where id ={Id}";
+                    DbConnector.Execute(deleteQuery);
+                    Console.WriteLine("I,ve terminated that user, goodbye");
+                }
+            }
+            else{
+                Console.WriteLine("User you do not exist or I cannot do that");
+            }
+
+        //write== inline block; write line == block
+
+            Console.Write("Please enter your first name: ");
+            first = Console.ReadLine(); //this allows the user to input informatiion. turns info into a string
+            Console.WriteLine(); //this will print the line
+
+            Console.Write("Now enter your last name: ");
+            last = Console.ReadLine(); //this allows the user to input informatiion
+            Console.WriteLine(); //this will print the line
+
+            Console.Write("Finally, tell me your favorite number: ");
+            number = Console.ReadLine(); //this allows the user to input informatiion
+            Console.WriteLine(); //this will print the line
+
+            string x= $"INSERT INTO User(firstName, lastName, favoriteNum) VALUES('{first}', '{last}', '{number}')";
+            System.Console.WriteLine(x);
+            DbConnector.Execute(x);
+        }  
+        static void Main(string[] args)
+        {
+            // string InputLine = Console.ReadLine();
+            // DbConnector.Query("Some query string expecting data returned");
+            Read();
+            Create();
+        }
+
+        }
+}
+///.ToList... .ToString
--- music-linq/Program.cs
+++ music-linq/Program.cs
@@ -18,18 +18,56 @@ namespace ConsoleApplication
             //========================================================
 
             //There is only one artist in this collection from Mount Vernon, what is their name and age?
-            Artist firstArtist = new Artist();
-            Console.WriteLine(firstArtist.Hometown);
+            Artist firstArtist = Artists.Where(artist => artist.Hometown == "Mount Vernon").Single();
+            Console.WriteLine(firstArtist.ArtistName + " "+  firstArtist.Age);
 
             //Who is the youngest artist in our collection of artists?
+            Artist youngArtist = Artists.OrderBy(young => young.Age).First();
+            Console.WriteLine(youngArtist.ArtistName + " " + youngArtist.Age);
 
             //Display all artists with 'William' somewhere in their real name
-
+            IEnumerable <Artist> nameArtist = Artists.Where(name => name.RealName.Contains( "William"));
+            foreach(var artist_ in nameArtist)
+            {
+                Console.WriteLine(artist_.RealName + "" + artist_.ArtistName);
+            }
+            
             //Display the 3 oldest artist from Atlanta
+            IEnumerable <Artist> oldArtist = Artists.OrderByDescending(old => old.Age).Take(3);
+            foreach(var aged in oldArtist)
+            {
+                Console.WriteLine(aged.ArtistName);
+            }
+
 
             //(Optional) Display the Group Name of all groups that have members that are not from New York City
-
+            // IEnumerable <Group> groupme = Groups.Join(Artists, Groups)
+            var noNewYork = (from band in Groups
+                            join artist in Artists on band.Id equals artist.GroupId
+                            where artist.Hometown != "New York"
+                            select new {band.GroupName}).Distinct();
+            foreach (var artistGroup in noNewYork )
+            {
+                Console.WriteLine(artistGroup.GroupName);
+            }
             //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+            // var WuTang = Group.Where(Group => Group.GroupName == "Wu-Tang Clan"); //Platform's way in solutions
+            //             .GroupJoin(Artists,
+            //             group => group.ID, 
+            //             artist => artist.GroupId, 
+            //             (group, artists) => {group.Members = artists.ToList(); 
+            //             return group});
+            //             .Single();
+
+            //     Console.WriteLine("List of Artist in the Wu-Tang Clam");
+            //     foreach(var artist in WuTang.Members){
+            //         Console.WriteLine(artist.Members);
+            //     }
+            IEnumerable <Group> groupArtist = Groups.Where(artistingroup_ => artistingroup_.GroupName == "Wu-Tang Clan").Take(5);
+            foreach (var artist in groupArtist)
+            {
+                Console.WriteLine(artist.Members);
+            }
         }
     }
 }
